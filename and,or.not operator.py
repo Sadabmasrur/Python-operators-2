41880d766f5f364ff2dc72e263e8ef6201407c38
Sadab Masrur <sadabmasrur08@gmail.com>
2024-12-28 11:44:49 +0600

    first commit

---
+++ and,or.not operator.py
@@ -0,0 +1,27 @@
+a=23
+b=14
+c=26
+
+if a>b or a<c:
+    print("I am using OR operator")
+    
+elif a<b and a>c: 
+    print("I am using AND operator")   
+    
+    
+x=None
+
+if not x:
+    print("X does not exists")
+    
+else:
+    print("X has a value")
+    
+    
+a=21
+b=43
+c=43
+
+print(a!=b) 
+print(b!=c)       
+        
