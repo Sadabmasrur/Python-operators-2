eb7c8db509274b224adb2e627fb45a7da2df26f8
Sadab Masrur <sadabmasrur08@gmail.com>
2024-12-30 10:16:29 +0600

    acp

---
+++ acp.py
@@ -0,0 +1,12 @@
+char = input("Enter a Character: ")
+
+if char >= "a" and char <= "z":
+    
+    print(f"The character {char} is a LowerCase Alphabet")
+    
+elif char >= "A" and char <= "Z":
+    
+    print(f"The character {char} is an UpperCase Alphabet")
+    
+else:
+    print(f"The character {char} is NOTTT an Alphabet")
